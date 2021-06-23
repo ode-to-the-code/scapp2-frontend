f57041adeb7867705f3c1742c3a4c99defb34b2a
jad abdallah <justcallmethis@gmail.com>
2021-06-23 14:18:29 -0700

    added first stuff for store in index.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react';
+
+
+class App extends React.Component {
+
+
+  componentDidMount(){
+    fetch('http://localhost:3000/api/v1/incidents/1/entries')
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }
+
+
+
+
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      App
     </div>
   );
+
+    }
 }
+
+
 
 export default App;
